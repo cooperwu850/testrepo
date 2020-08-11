146c421115ad6b99bf394eaee295bceae77f0546
cooperwu850 <69314538+cooperwu850@users.noreply.github.com>
2020-08-11 21:56:32 +0800

    Add files via upload

---
+++ open.py.py
@@ -0,0 +1,11 @@
+file1=open(r'C:\Users\Administrator\Desktop\test\1.txt','w')
+file1.write('难念的经')
+file1.close()
+
+file1=open(r'C:\Users\Administrator\Desktop\test\1.txt','r')
+print(file1.read())
+file1.close()
+
+
+
+
